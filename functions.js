ab3e8ceade6d0ce5095caf45737a1210052ff2c4
Icha <effroi@yahoo.fr>
2013-08-27 18:36:17 +0200

    add function to add styles with media queries in header of the page

--- functions.js
+++ functions.js
@@ -59,3 +59,39 @@ addCssRuleToHead({
 		'background-color': '#000'
 	}
 });
+
+/**
+ * Ajouter des styles en dynamique à l'intérieur d'un media querie
+ */
+function addCssRuleWithMediaQueryToHead (cssRule) {
+    var head = document.getElementsByTagName('head')[0],
+        s = document.createElement('style') ;
+    css = '' ;
+    for (var cssrulename in cssRule) {
+        if (!cssRule.hasOwnProperty(cssrulename) || !cssrulename in cssRule) continue ;
+        css += cssrulename+'{' ;
+        for (var cssattributes in cssRule[cssrulename]) {
+            css += cssattributes+': '+cssRule[cssrulename][cssattributes]+';' ;
+        }
+        css+='}';
+    }
+    s.setAttribute('type', 'text/css');
+    if (s.styleSheet) { // IE
+        s.styleSheet.cssText = css;
+    } else { // the world
+        s.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(s);
+}
+
+addCssRuleWithMediaQueryToHead('@media screen and (max-width: 640px) and (orientation:portrait)',{
+    '#lbSpacer': {
+        'height': '0'
+    },
+    '.homeContHl': {
+        'margin-top': '-14px'
+    },
+    'body': {
+        'background-color': '#000'
+    }
+});
